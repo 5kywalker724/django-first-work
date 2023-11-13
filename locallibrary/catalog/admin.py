6038eadd4c4a8758ab196953c40d555778113d94
PinT271 <olezhka.kuzmin.99@inbox.ru>
2023-11-13 19:34:11 +0700

    Добавление разделений на секции для BookInstance в админ-панели

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -25,3 +25,12 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
 
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
