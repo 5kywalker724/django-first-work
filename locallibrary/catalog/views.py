2016e0e97cc75a9d6502820a1358dafc03798445
Олег Кузьмин <gr412_kuoig@lab-407-8.tomtit-tomsk.ru>
2023-11-14 12:38:15 +0700

    Добавление подсчёта визитов

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -9,8 +9,11 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()  # Метод .all() применяется по умолчанию
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
     return render(request, 'index.html', context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available,
-                                                  'num_authors': num_authors})
+                                                  'num_authors': num_authors, 'num_visits': num_visits})
 
 
 class BookListView(generic.ListView):
