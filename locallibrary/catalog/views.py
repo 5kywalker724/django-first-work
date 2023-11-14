8657cef1df8c1c95b724628aa204b6d54a501427
PinT271 <olezhka.kuzmin.99@inbox.ru>
2023-11-14 22:50:32 +0700

    Добавление тестов представления

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -29,15 +29,16 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 10
 
 
 class BookDetailView(generic.DetailView):
     model = Book
-    paginate_by = 10
 
 
 class AuthorListView(generic.ListView):
     model = Author
+    paginate_by = 10
 
 
 class AuthorDetailView(generic.DetailView):
