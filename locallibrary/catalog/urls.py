b7d2635417ea430b21949171011452ee4d3845f9
PinT271 <olezhka.kuzmin.99@inbox.ru>
2023-11-14 18:32:13 +0700

    Добавление списка пользователей и их книг

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('allbooks/', views.AllLoanedBooksByUserListView.as_view(), name='all-borrowed'),
 ]
